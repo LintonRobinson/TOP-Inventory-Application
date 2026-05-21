d5a49c789384fbe3d36ad6eea7e39c375273a9cd
LintonRobinson <40444726+LintonRobinson@users.noreply.github.com>
2026-05-21 17:42:07 -0400

    feat: add insertItem query

--- db/queries.js
+++ db/queries.js
@@ -18,9 +18,22 @@ async function updateCategory(categoryId, categoryName, categoryDescription) {
   await pool.query("UPDATE categories SET name = $2, description = $3 WHERE id = $1", [categoryId, categoryName, categoryDescription]);
 }
 
+async function insertItem(categoryId, validatedItem) {
+  await pool.query("INSERT INTO items (category,name, quantity,price,description,notes,url) VALUES ($1,$2,$3,$4,$5,$6,$7)", [
+    validatedItem.itemCategory,
+    validatedItem.itemName,
+    validatedItem.itemQuantity,
+    validatedItem.itemPrice,
+    validatedItem.itemDescription,
+    validatedItem.itemNotes,
+    validatedItem.itemUrl,
+  ]);
+}
+
 module.exports = {
   insertCategory,
   updateCategory,
   getCategories,
   getCategory,
+  insertItem,
 };
