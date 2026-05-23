39213592babc8bd0ab607b7925cc9562ef095756
LintonRobinson <40444726+LintonRobinson@users.noreply.github.com>
2026-05-23 18:17:39 -0400

    feat: add category view route with filtered items

--- routes/categoryRouter.js
+++ routes/categoryRouter.js
@@ -15,6 +15,14 @@ categoryRouter.get("/edit/:id", async (req, res) => {
   res.render("editCategory", { category: category });
 });
 
+categoryRouter.get("/:id", async (req, res) => {
+  const categoryId = req.query.categoryToView;
+  const categories = await db.getCategories();
+  const items = await db.getCategoryItems(categoryId);
+  console.log("items", items);
+  res.render("index", { title: "A different Cat", description: "All Shiraz Farm items.", categories: categories, categoryId: categoryId, items: items });
+});
+
 categoryRouter.post("/edit/:categoryId", categoryController.validateCategory, categoryController.updateCategory);
 
 categoryRouter.post("/new", categoryController.validateCategory, categoryController.insertCategory);
